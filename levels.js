e49f2ae465112e62cb7aa62bc7aa09526e31888b
Mahmoud <leedor97@gmail.com>
2018-01-19 13:14:29 +0200

    BrickBreaker Advanced coding

---
+++ levels.js
@@ -0,0 +1,84 @@
+var level1 = {
+        beck1: { x: 180, y: 150, width: 180, height: 40,image: 0, color: "blue",  score: 10 },
+        beck2: { x: 430, y: 150, width: 180, height: 40,image: 0, color: "blue",  score: 10 },
+        beck3: { x: 680, y: 150, width: 180, height: 40,image: 1,  color: "red",  score: 15 }
+}
+
+var level2 = {
+        beck5: { x: 150, y: 150, width: 90, height: 40, image: 0, color: "blue",  score: 10 },
+        beck6: { x: 250, y: 150, width: 90, height: 40, image: 1, color: "blue",  score: 10 },
+        beck7: { x: 350, y: 150, width: 90, height: 40, image: 0, color: "blue",  score: 10 },
+        beck8: { x: 450, y: 150, width: 90, height: 40, image: 0, color: "blue",  score: 10 },
+        beck9: { x: 550, y: 150, width: 90, height: 40, image: 0, color: "blue",  score: 10 },
+        beck10: { x: 650, y: 150, width: 90, height: 40, image: 0, color: "blue",  score: 10 },
+        beck11: { x: 750, y: 150, width: 90, height: 40, image: 0, color: "blue",  score: 10 },
+        beck15: { x: 250, y: 200, width: 90, height: 40, image: 1, color: "red",  score: 15 },
+        beck17: { x: 450, y: 200, width: 90, height: 40, image: 0, color: "red",  score: 15 },
+        beck19: { x: 650, y: 200, width: 90, height: 40, image: 0, color: "red",  score: 15 },
+        beck20: { x: 550, y: 200, width: 90, height: 40, image: 0, color: "red",  score: 15 },
+        beck21: { x: 350, y: 200, width: 90, height: 40, image: 0, color: "red",  score: 15 },
+        beck22: { x: 250, y: 200, width: 90, height: 40, image: 1, color: "red",  score: 15 },
+        beck28: { x: 295, y: 250, width: 90, height: 40, image: 1, color: "green",  score: 20 },
+        beck29: { x: 395, y: 250, width: 90, height: 40, image: 0, color: "green",  score: 20 },
+        beck30: { x: 495, y: 250, width: 90, height: 40, image: 0, color: "green",  score: 20 },
+        beck31: { x: 595, y: 250, width: 90, height: 40, image: 0, color: "green",  score: 20 }
+
+}
+
+var level3 = {
+        beck5: { x: 150, y: 150, width: 90, height: 40, image: 0, color: "green",  score: 20 },
+        beck6: { x: 250, y: 150, width: 90, height: 40, image: 1, color: "green",  score: 20 },
+        beck7: { x: 350, y: 150, width: 90, height: 40, image: 0, color: "green",  score: 20 },
+        beck8: { x: 450, y: 150, width: 90, height: 40, image: 0, color: "green",  score: 20 },
+        beck9: { x: 550, y: 150, width: 90, height: 40, image: 0, color: "green",  score: 20 },
+        beck10: { x: 650, y: 150, width: 90, height: 40, image: 0, color: "green",  score: 20 },
+        beck11: { x: 750, y: 150, width: 90, height: 40, image: 0, color: "green",  score: 20 },
+        beck15: { x: 250, y: 100, width: 90, height: 40, image: 1, color: "red",  score: 15 },
+        beck17: { x: 450, y: 100, width: 90, height: 40, image: 0, color: "red",  score: 15 },
+        beck19: { x: 650, y: 100, width: 90, height: 40, image: 0, color: "red",  score: 15 },
+        beck20: { x: 550, y: 200, width: 90, height: 40, image: 0, color: "red",  score: 15 },
+        beck21: { x: 350, y: 200, width: 90, height: 40, image: 0, color: "red",  score: 15 },
+        beck1: { x: 150, y: 250, width: 90, height: 40, image: 0, color: "blue",  score: 10 },
+        beck2: { x: 250, y: 250, width: 90, height: 40, image: 1, color: "blue",  score: 10 },
+        beck3: { x: 350, y: 250, width: 90, height: 40, image: 0, color: "blue",  score: 10 },
+        beck4: { x: 450, y: 250, width: 90, height: 40, image: 0, color: "blue",  score: 10 },
+        beck23: { x: 550, y: 250, width: 90, height: 40, image: 0, color: "blue",  score: 10 },
+        beck24: { x: 650, y: 250, width: 90, height: 40, image: 0, color: "blue",  score: 10 },
+        beck25: { x: 750, y: 250, width: 90, height: 40, image: 0, color: "blue",  score: 10 }
+}
+
+var level4 = {
+        beck5: { x: 150, y: 150, width: 90, height: 40, image: 0, color: "blue",  score: 10 },
+        beck6: { x: 250, y: 150, width: 90, height: 40, image: 1, color: "blue",  score: 15 },
+        beck7: { x: 350, y: 150, width: 90, height: 40, image: 0, color: "blue",  score: 10 },
+        beck8: { x: 450, y: 150, width: 90, height: 40, image: 0, color: "blue",  score: 10 },
+        beck9: { x: 550, y: 150, width: 90, height: 40, image: 0, color: "blue",  score: 10 },
+        beck10: { x: 650, y: 150, width: 90, height: 40, image: 0, color: "blue",  score: 10 },
+        beck11: { x: 750, y: 150, width: 90, height: 40, image: 0, color: "blue",  score: 10 },
+        beck15: { x: 250, y: 100, width: 90, height: 40, image: 1, color: "red",  score: 15 },
+        beck17: { x: 450, y: 100, width: 90, height: 40, image: 0, color: "red",  score: 15 },
+        beck19: { x: 650, y: 100, width: 90, height: 40, image: 0, color: "red",  score: 15 },
+        beck20: { x: 550, y: 200, width: 90, height: 40, image: 0, color: "red",  score: 15 },
+        beck21: { x: 350, y: 200, width: 90, height: 40, image: 0, color: "red",  score: 15 },
+        beck22: { x: 250, y: 250, width: 90, height: 40, image: 1, color: "red",  score: 15 },
+        beck23: { x: 450, y: 250, width: 90, height: 40, image: 0, color: "red",  score: 15 },
+        beck24: { x: 650, y: 250, width: 90, height: 40, image: 0, color: "red",  score: 15 },
+        beck25: { x: 550, y: 250, width: 90, height: 40, image: 0, color: "red",  score: 15 },
+        beck26: { x: 350, y: 250, width: 90, height: 40, image: 0, color: "red",  score: 15 },
+        beck27: { x: 150, y: 300, width: 90, height: 40, image: 0, color: "green",  score: 20 },
+        beck28: { x: 250, y: 300, width: 90, height: 40, image: 1, color: "green",  score: 20 },
+        beck29: { x: 350, y: 300, width: 90, height: 40, image: 0, color: "green",  score: 20 },
+        beck30: { x: 450, y: 300, width: 90, height: 40, image: 0, color: "green",  score: 20 },
+        beck31: { x: 550, y: 300, width: 90, height: 40, image: 0, color: "green",  score: 20 },
+        beck32: { x: 650, y: 300, width: 90, height: 40, image: 0, color: "green",  score: 20 },
+        beck33: { x: 750, y: 300, width: 90, height: 40, image: 0, color: "green",  score: 20 }
+}
+
+
+
+
+
+
+
+
+
